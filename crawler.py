970d5a6d7f4f705e34f757dba5a7d8546cb110bf
pr3ssh <pablo.martin@enreda.coop>
2018-02-28 13:27:46 +0100

    Adding pagination

--- crawler.py
+++ crawler.py
@@ -25,33 +25,41 @@ def get_data_safely(list_, index, default):
 
 ## Codigo de la mision
 
-req  = requests.get("https://www.idealista.com/alquiler-viviendas/valencia/poblats-maritims/el-cabanyal-el-canyamelar/")
-data = req.text
-soup = BeautifulSoup(data, "html.parser")
+root_url = "https://www.idealista.com"
+active_url = "/alquiler-viviendas/valencia/poblats-maritims/el-cabanyal-el-canyamelar/"
 idealista_houses = list()
-houses = soup.find_all("div", class_="item")
-# OTRA OPCION: houses = soup_find_all('element', {'class': ""})
-for house in houses:
-    item_link = house.find("a", class_="item-link")
-    name = item_link.text
-    url = item_link['href']
-    price = house.find("span", class_="item-price").text
-    details = house.find_all("span", class_="item-detail")
-    rooms = get_data_safely(details, 0, "")
-    size = get_data_safely(details, 1, "")
-    moreinfo = get_data_safely(details, 2, "")
-    try:
-        phone = house.find("a", class_="item-clickable-phone").text
-    except AttributeError:
-        phone = ""
-    idealista_houses.append({
-        'name': name,
-        'url': url,
-        'price': price,
-        'rooms': rooms,
-        'size': size,
-        'moreinfo': moreinfo,
-        'phone': phone
-    })
+while True:
+    req  = requests.get(root_url+active_url)
+    data = req.text
+    soup = BeautifulSoup(data, "html.parser")
+    houses = soup.find_all("div", class_="item")
+    # OTRA OPCION: houses = soup_find_all('element', {'class': ""})
+    for house in houses:
+        item_link = house.find("a", class_="item-link")
+        name = item_link.text
+        url = item_link['href']
+        price = house.find("span", class_="item-price").text
+        details = house.find_all("span", class_="item-detail")
+        rooms = get_data_safely(details, 0, "")
+        size = get_data_safely(details, 1, "")
+        moreinfo = get_data_safely(details, 2, "")
+        try:
+            phone = house.find("a", class_="item-clickable-phone").text
+        except AttributeError:
+            phone = ""
+        idealista_houses.append({
+            'name': name,
+            'url': url,
+            'price': price,
+            'rooms': rooms,
+            'size': size,
+            'moreinfo': moreinfo,
+            'phone': phone
+        })
+    next_url = soup.find("a", class_="icon-arrow-right-after")
+    if not next_url:
+        break
+    active_url = next_url["href"]
+    
 
 print(json.dumps(idealista_houses, indent=4))
